a6f5b9b4a477cbdd6ea0ee85bf93769bec141ea5
Nitish236 <68368233+Nitish236@users.noreply.github.com>
2023-08-04 15:18:33 +0530

    Email Service
    Wrote the email service, there are two methods :-
    1. One is to send the email of joining.
    2. Other is to send the email with attachments, it will be used to send reports to the Manager's and Super Admin's.

---
+++ utils/mailService/email.js
@@ -0,0 +1,86 @@
+require("dotenv").config();
+
+// Template engine
+const hbs = require("nodemailer-express-handlebars");
+const nodemailer = require("nodemailer");
+const path = require("path");
+const { MailError } = require("../../errors/mailError");
+
+// Selecting the Service
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: true,
+  auth: {
+    user: process.env.IAM_MAIL_USER,
+    pass: process.env.IAM_MAIL_PASSWORD,
+  },
+});
+
+const handlebarOptions = {
+  viewEngine: {
+    partialsDir: path.resolve("./views/"),
+    defaultLayout: false,
+  },
+  viewPath: path.resolve("./views/"),
+};
+
+// use a template file with nodemailer
+transporter.use("compile", hbs(handlebarOptions));
+
+// Send Email (Login Credentials)
+
+const sendEmail = async (user) => {
+  let emailOptions = {
+    from: process.env.IAM_MAIL,
+    to: user.email,
+    subject: "Welcome Mail",
+    template: "email",
+    context: user.context,
+  };
+
+  transporter.sendMail(emailOptions, async (error) => {
+    if (error) {
+      throw new MailError("Something wrong happened while Sending Mail");
+    }
+  });
+};
+
+// Send Email (With Attachments)
+
+const sendEmailWithAttachement = async (user) => {
+  const dir = path.resolve(__dirname, "../../");
+  const filePath = path.join(dir, user.filePath);
+
+  let emailOptions = {
+    from: process.env.IAM_MAIL,
+    to: user.email,
+    subject: "Weekly Report Mail",
+    template: "email",
+    context: {
+      department: user.department,
+      attachment: true,
+    },
+    attachments: [
+      {
+        path: filePath,
+      },
+    ],
+  };
+
+  // Send Email
+  transporter.sendMail(emailOptions, async (error) => {
+    if (error) {
+      console.log(error);
+      throw new MailError("Something wrong happened while Sending Mail");
+    }
+  });
+};
+
+// Export Functionality
+
+module.exports = {
+  sendEmail,
+  sendEmailWithAttachement,
+};
